b3a27e62595eb284f71b01513a9d2c58307f3706
ayemyatmyatphyo <ayemyatmyatphyo@mm.cybermissions.co.jp>
2021-09-02 16:37:59 +0900

    Complete Advanced Setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# test result にSuccessならGreenに表示して、エラーならRedに表示するColorつける
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
