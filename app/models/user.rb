98db76d52798c409be8e518827bd2b999bbf1b34
ayemyatmyatphyo <ayemyatmyatphyo@mm.cybermissions.co.jp>
2021-09-15 19:01:11 +0900

    Sigup作成と登録追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
     format: { with: VALID_EMAIL_REGEX } , uniqueness: { case_sensitive: false }
     before_save { email.downcase! }
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6}
 end
