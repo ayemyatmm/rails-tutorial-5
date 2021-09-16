92bb41ff06ae75b753c0cc834b1175ce315fcb62
ayemyatmyatphyo <ayemyatmyatphyo@mm.cybermissions.co.jp>
2021-09-16 19:04:21 +0900

    basic-login追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-# test result にSuccessならGreenに表示して、エラーならRedに表示するColorつける
 require "minitest/reporters"
 Minitest::Reporters.use!
 
